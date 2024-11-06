acf02f0f0262798ac80d36554780bf43aafd0ea5
Anshu Ojha <oanshu600@gmail.com>
2024-11-06 18:37:50 +0530

    Redux functionality usage

---
+++ table-components/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import DataList from './Components/table';
+
+
+function App() {
+  return (
+    <div className="App p-sm border-cyan-900" >
+       <addMember />
+      <DataList />
+    </div>
+  );
+}
+
+export default App;
